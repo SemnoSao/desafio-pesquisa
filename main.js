9d1b439900c9da9ecd836fd0191ade97012159e1
SemnoSao <arthur.caccavo@gmail.com>
2022-04-07 14:11:09 -0300

    alt do format dos cards, sup a multi cards

--- main.js
+++ main.js
@@ -4,23 +4,36 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const pesquisa = document.getElementById("pesquisa");
 
-getRepo("Grimorio+TRPG")
+getRepo("tetris")
 
 async function getRepo(termo_pesquisa) {
     const resp = await fetch(API_URL + termo_pesquisa);
     const respJSON = await resp.json();
 
-    criarCard(respJSON.items[0]);
+    respJSON.items.forEach(criarCard);
 }
 
 function criarCard(repo) {
+    const data_nao_formatada = new Date(repo.updated_at);
+    const offsetMs = data_nao_formatada.getTimezoneOffset() * 60 * 1000;
+    const data_local = new Date(data_nao_formatada.getTime() - offsetMs);
+    const data = data_local.toISOString().slice(0, 19).replace(/-/g, "-").replace("T", " ");
+
+    var descri_formatada = new String(repo.description).replace(/(\r\n|\n|\r)/gm, " ");
+    if (descri_formatada.valueOf() != new String("null").valueOf()){
+        if (descri_formatada.length > 280){
+            descri_formatada = descri_formatada.substring(0, 280).concat("[...]");
+        }
+    }
+    else descri_formatada = "";
+
     const cardHTML = `
     <div class="card">
-        <h2>${repo.full_name}</h2>
-        <p>${repo.description}</p>
+        <h2><a href="${repo.html_url}" target="_blank" >${repo.name}</a></h2>
+        <p>${descri_formatada} por ${repo.owner.login}</p>
         <h3>${repo.language}</h3>
         <h4>${repo.stargazers_count} stars ${repo.forks_count} forks</h4>
-        <p> editado em: ${repo.updated_at} <\p>
+        <p> editado em: ${data} <\p>
     </div>
   `
 
